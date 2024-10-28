598bb1d6b8477884dbb19ea179bf6b667ccad141
Quiyet Brul <qbrul0514@gmail.com>
2024-10-28 00:55:14 -0600

    start composite

---
+++ src/section2/StructuralDesign/Composite/composite.go
@@ -0,0 +1,14 @@
+package composite
+
+/*
+The Composite pattern is a structural design pattern that
+allows to compose objects into tree structures to
+represent part-whole hierarchies.
+This pattern lets clients treat individual objects
+and compositions of objects uniformly.
+
+- helps to shape applications with commonly used structures and relationships
+  - composition "has-a" relationship, inheritance "is-a" relationship
+- golang encourages use of composition almost exclusively by its lack of inheritance
+*/
+
