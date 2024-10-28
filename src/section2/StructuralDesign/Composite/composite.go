c5a3a3e48eefd9b98a2936d4dc3fbafa83de9c2d
Quiyet Brul <qbrul0514@gmail.com>
2024-10-28 16:40:17 -0600

    Update composite.go

--- src/section2/StructuralDesign/Composite/composite.go
+++ src/section2/StructuralDesign/Composite/composite.go
@@ -10,5 +10,42 @@ and compositions of objects uniformly.
 - helps to shape applications with commonly used structures and relationships
   - composition "has-a" relationship, inheritance "is-a" relationship
 - golang encourages use of composition almost exclusively by its lack of inheritance
+
+- TWO TYPES OF COMPOSITION
+  - direct composition
+  - embedding composition
 */
 
+type Athlete struct{}
+
+func (a *Athlete) Train() {
+	println("Training")
+}
+
+func Swim() {
+	println("Swimming!")
+}
+
+type CompositeSwimmerA struct {
+	MyAthlete Athlete
+	MySwim    *func()
+}
+
+type Trainer interface {
+	Train()
+}
+
+type Swimmer interface {
+	Swim()
+}
+
+type SwimmerImplementor struct{}
+
+func (s *SwimmerImplementor) Swim() {
+	println("Swimming!")
+}
+
+type CompositeSwimmerB struct {
+	Trainer
+	Swimmer
+}
